e359d0c96a9f0bf9c7517c29ffd626ccdb485d34
songcw8 <ben990158@gmail.com>
2025-04-22 11:10:36 +0900

    swagger, springdoc

---
+++ src/main/java/org/example/bootswagger/controller/testController.java
@@ -0,0 +1,26 @@
+package org.example.bootswagger.controller;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/api/users")
+@Tag(name = "Hi")
+public class testController {
+
+    @GetMapping
+    public String test() {
+        return "hello world";
+    }
+
+    @GetMapping("/welcome")
+    @ApiResponse(responseCode = "204")
+    public ResponseEntity<Void> welcome() {
+        return ResponseEntity.noContent().build();
+    }
+
+}
