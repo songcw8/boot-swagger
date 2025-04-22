1ee68fc32f2c98cbb7f196f5889fa010280c0add
songcw8 <ben990158@gmail.com>
2025-04-22 10:05:17 +0900

    Initial commit

---
+++ src/test/java/org/example/bootswagger/BootSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
