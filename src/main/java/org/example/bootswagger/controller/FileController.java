8da6ce30e9e0ef639c3cdf808d534c207880166d
songcw8 <ben990158@gmail.com>
2025-04-22 12:41:10 +0900

    supabase storage pdf, img, xls 업로드 기능

--- src/main/java/org/example/bootswagger/controller/FileController.java
+++ src/main/java/org/example/bootswagger/controller/FileController.java
@@ -1,6 +1,7 @@
 package org.example.bootswagger.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.bootswagger.service.StorageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +10,18 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RequestMapping("/api/file")
 public class FileController {
+    private final StorageService storageService;
+
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
     @PostMapping(consumes = "multipart/form-data")
-    public ResponseEntity<Void> upload(@RequestParam("files") MultipartFile[] files) {
+    public ResponseEntity<Void> upload(@RequestParam("files") MultipartFile[] files) throws Exception {
         for (MultipartFile file : files) {
             log.info(file.getOriginalFilename());
+            String dbFileName = storageService.upload(file);
+            log.info(dbFileName);
         }
         return ResponseEntity.ok().build();
     }
--- src/main/java/org/example/bootswagger/service/StorageService.java
+++ src/main/java/org/example/bootswagger/service/StorageService.java
@@ -1,11 +1,19 @@
 package org.example.bootswagger.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -21,8 +29,46 @@ public class StorageService {
     @Value("${supabase.sheet-bucket-name}")
     private String sheetBucketName;
 
-
     public String upload(MultipartFile file) throws Exception {
-        return "";
+        String contentType = Objects.requireNonNull(file.getContentType());
+        String uuid = UUID.randomUUID().toString();
+        String fileName = file.getOriginalFilename();
+        String extension = Objects.requireNonNull(fileName).substring(fileName.lastIndexOf(".") + 1);
+        String boundary = "Boundary-%s".formatted(uuid);
+        String filename = "%s.%s".formatted(uuid, extension);
+        String bucketName;
+        if (extension.equals("pdf")) {
+            bucketName = pdfBucketName;
+        } else if (extension.equals("xls") || extension.equals("xlsx")) {
+            bucketName = sheetBucketName;
+        } else if (contentType.contains("image")) {
+            bucketName = imgBucketName;
+        } else {
+            // 다른 파일은?
+            throw new BadRequestException("허용되지 않는 파일");
+        }
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("%s/storage/v1/object/%s/%s"
+                        .formatted(url, bucketName, filename)))
+                .header("Authorization", "Bearer %s".formatted(accessKey))
+                .header("Content-Type", "multipart/form-data; boundary=%s".formatted(boundary))
+                .POST(ofMimeMultipartData(file, boundary))
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new Exception(response.body());
+        }
+        return filename;
+    }
+
+    private HttpRequest.BodyPublisher ofMimeMultipartData(MultipartFile file, String boundary) throws IOException {
+        List<byte[]> byteArrays = List.of(
+                ("--" + boundary + "\r\n" +
+                        "Content-Disposition: form-data; name=\"file\"; filename=\"" + file.getOriginalFilename() + "\"\r\n" +
+                        "Content-Type: " + file.getContentType() + "\r\n\r\n").getBytes(),
+                file.getBytes(),
+                ("\r\n--" + boundary + "--\r\n").getBytes()
+        );
+        return HttpRequest.BodyPublishers.ofByteArrays(byteArrays);
     }
 }
